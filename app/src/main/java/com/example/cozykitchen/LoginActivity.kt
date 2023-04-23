700d2e19e56cfa903b51c58e1a29734ee9733f3e
Frankyfoo <frankyfoo92@gmail.com>
2023-04-23 16:30:07 +0800

    Created UI for Registration and login

--- app/src/main/java/com/example/cozykitchen/LoginActivity.kt
+++ app/src/main/java/com/example/cozykitchen/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.example.cozykitchen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // hide action bar
+        supportActionBar?.hide()
         setContentView(R.layout.activity_login)
     }
 }
--- app/src/main/java/com/example/cozykitchen/SplashActivity.kt
+++ app/src/main/java/com/example/cozykitchen/SplashActivity.kt
@@ -15,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
             // This method will be executed once the timer is over
             // Start your app main activity
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
 
             finish()
         }, SPLASH_TIME_OUT)
