24b9febfde7d0d2652a3358425b61ff7cb64eda3
Frankyfoo <frankyfoo92@gmail.com>
2023-04-25 16:50:12 +0800

    Added ViewBinding and OnclickListener to move between registration and login.

--- app/src/main/java/com/example/cozykitchen/LoginActivity.kt
+++ app/src/main/java/com/example/cozykitchen/LoginActivity.kt
@@ -1,13 +1,29 @@
 package com.example.cozykitchen
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cozykitchen.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         // hide action bar
         supportActionBar?.hide()
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvRegister.setOnClickListener {
+            toRegisterActivity()
+        }
+    }
+
+    private fun toRegisterActivity() {
+        val intent = Intent(this, RegistrationActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/cozykitchen/RegistrationActivity.kt
+++ app/src/main/java/com/example/cozykitchen/RegistrationActivity.kt
@@ -1,11 +1,30 @@
 package com.example.cozykitchen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cozykitchen.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+
+        // hide action bar
+        supportActionBar?.hide()
+
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvLogin.setOnClickListener {
+            toLoginActivity()
+        }
+    }
+
+    private fun toLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        startActivity(intent)
     }
 }
